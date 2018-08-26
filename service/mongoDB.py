8b6334ae3b93403bd90c0e7646b9e46560869429
yuyilei <yyl1352091742@gmail.com>
2018-08-26 16:26:56 +0800

    change mongodb

--- service/mongoDB.py
+++ service/mongoDB.py
@@ -7,6 +7,6 @@ MONGODB_PORT = int(os.getenv('MONGODB_PORT') or '27017')
 
 async def db_setup():
     client = AsyncIOMotorClient(MONGODB_HOST, MONGODB_PORT)
-    lessonset = client['lessonset']
-    lessondb = lessonset['lessondb']
+    lessonset = client['lessonset2018']
+    lessondb = lessonset['lessondb2018']
     return lessondb
