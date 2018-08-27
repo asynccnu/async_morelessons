5640d6a08d8e7f6c422b344dd1db4c1fa03c81b2
yuyilei <yyl1352091742@gmail.com>
2018-08-27 09:56:15 +0800

    fix: update mongodb set and collection to env

--- service/mongoDB.py
+++ service/mongoDB.py
@@ -3,10 +3,12 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 MONGODB_HOST = os.getenv('MONGODB_HOST') or 'localhost'
 MONGODB_PORT = int(os.getenv('MONGODB_PORT') or '27017')
+LESSONSET = os.getenv('LESSONSET')
+LESSONDB = os.getenv('LESSONDB')
 
 
 async def db_setup():
     client = AsyncIOMotorClient(MONGODB_HOST, MONGODB_PORT)
-    lessonset = client['lessonset2018']
-    lessondb = lessonset['lessondb2018']
+    lessonset = client[LESSONSET]
+    lessondb = lessonset[LESSONDB]
     return lessondb
